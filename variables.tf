d87910c95661547a633cb9c1233838ac8185b4b8
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2022-07-05 14:59:12 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable aws_region {
 variable project_tag {
   description = "Value of the 'Project' tag for all resources"
   type        = string
-  default     = "learn-terraform-run-triggers"
+  default     = "terraform-run-triggers"
 }
 
 variable vpc_cidr_block {
